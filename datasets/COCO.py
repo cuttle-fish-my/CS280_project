04486c9704ceed2933ae8e51a362e3e295752ce8
libn <libn@shanghaitech.edu.cn>
2023-05-31 22:34:26 +0800

    add two stage dataset

--- datasets/COCO.py
+++ datasets/COCO.py
@@ -7,22 +7,53 @@ import cv2
 import pickle
 
 
-def CategoryImageFileNamePickle(json_path):
-    mode = os.path.basename(json_path).split(".")[0].split("_")[1][:-4]
-    coco = COCO(json_path)
-    cats = coco.loadCats(coco.getCatIds())
-    names = [cat['name'] for cat in cats]
-    pickle.dump(names, open("COCO/categories.pkl", "wb"))
+def labelIdMap(label_list_dir, names):
+    label_id_map = {}
+    with open(label_list_dir, "r") as f:
+        while True:
+            l = f.readline().split('\n')[0]
+            if not l:
+                break
+            id = l.split(':')[0]
+            category = l.split(':')[1][1:]
+            if category in names:
+                label_id_map[category] = int(id) - 1
+            else:
+                print(f"{category} not found!")
+    return label_id_map
+
+def CategoryImageFileNamePickle(thing_json_dir, stuff_json_dir, label_list_dir):
+    mode = os.path.basename(stuff_json_dir).split(".")[0].split("_")[1][:-4]
+    stuff_coco = COCO(stuff_json_dir)
+    thing_coco = COCO(thing_json_dir)
+    stuff_cats = stuff_coco.loadCats(stuff_coco.getCatIds())
+    thing_cats = thing_coco.loadCats(thing_coco.getCatIds())
+    thing_names = [cat['name'] for cat in thing_cats]
+    stuff_names = [cat['name'] for cat in stuff_cats]
+    names = thing_names + stuff_names
+    names.remove("other")
+    label_id_map = labelIdMap(label_list_dir, names)
+    pickle.dump(label_id_map, open("COCO/categories.pkl", "wb"))
     filename_pickle = {}
     for name in names:
-        catIds = coco.getCatIds(catNms=[name])
-        imgIds = coco.getImgIds(catIds=catIds)
-        imgs = coco.loadImgs(imgIds)
+        if name in thing_names:
+            catIds = thing_coco.getCatIds(catNms=[name])
+            imgIds = thing_coco.getImgIds(catIds=catIds)
+            imgs = thing_coco.loadImgs(imgIds)
+        elif name in stuff_names:
+            catIds = stuff_coco.getCatIds(catNms=[name])
+            imgIds = stuff_coco.getImgIds(catIds=catIds)
+            imgs = stuff_coco.loadImgs(imgIds)
+        else:
+            raise ValueError
         filename_pickle[name] = [img['file_name'] for img in imgs]
     pickle.dump(filename_pickle, open(f"COCO/{mode}_filenames.pkl", "wb"))
 
 
 if __name__ == "__main__":
-    CategoryImageFileNamePickle("COCO/annotations/stuff_train2017.json")
-    CategoryImageFileNamePickle("COCO/annotations/stuff_val2017.json")
-
+    CategoryImageFileNamePickle("COCO/thing_annotations/instances_train2017.json",
+                                "COCO/stuff_annotations/stuff_train2017.json",
+                                "COCO/labels.txt")
+    CategoryImageFileNamePickle("COCO/thing_annotations/instances_val2017.json",
+                                "COCO/stuff_annotations/stuff_val2017.json",
+                                "COCO/labels.txt")
--- improved_diffusion/image_datasets.py
+++ improved_diffusion/image_datasets.py
@@ -1,12 +1,17 @@
+import os
+import pickle
+import random
+
 from PIL import Image
 import blobfile as bf
 from mpi4py import MPI
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
+import cv2
 
 
 def load_data(
-    *, data_dir, batch_size, image_size, class_cond=False, deterministic=False
+        *, data_dir, batch_size, image_size, class_cond=False, deterministic=False
 ):
     """
     For a dataset, create a generator over (images, kwargs) pairs.
@@ -97,10 +102,97 @@ class ImageDataset(Dataset):
         arr = np.array(pil_image.convert("RGB"))
         crop_y = (arr.shape[0] - self.resolution) // 2
         crop_x = (arr.shape[1] - self.resolution) // 2
-        arr = arr[crop_y : crop_y + self.resolution, crop_x : crop_x + self.resolution]
+        arr = arr[crop_y: crop_y + self.resolution, crop_x: crop_x + self.resolution]
         arr = arr.astype(np.float32) / 127.5 - 1
 
         out_dict = {}
         if self.local_classes is not None:
             out_dict["y"] = np.array(self.local_classes[idx], dtype=np.int64)
         return np.transpose(arr, [2, 0, 1]), out_dict
+
+
+class COCOCategoryDataset(Dataset):
+    def __init__(self, resolution, root, filename_pickle, label_id_map, num_support=5, train=True):
+        label_id_map = pickle.load(open(label_id_map, 'rb'))
+        self.datasets = []
+        for key, value in label_id_map.items():
+            self.datasets.append(
+                COCODataset(resolution, root, key, value, filename_pickle, num_support=num_support, train=train)
+            )
+
+    def __len__(self):
+        return len(self.datasets)
+
+    def __getitem__(self, idx):
+        return self.datasets[idx]
+
+
+class COCODataset(Dataset):
+    def __init__(self, resolution, root, category, category_id, filename_pickle, num_support=5, train=True):
+        super().__init__()
+        self.resolution = resolution
+        self.num_support = num_support
+        self.root = root
+        self.category = category
+        self.category_id = category_id
+        self.train = train
+        self.filename_list = pickle.load(open(filename_pickle, 'rb'))[category]
+
+    def __len__(self):
+        return len(self.filename_list)
+
+    def __getitem__(self, idx):
+        img, label = self._load_img_label(self.filename_list[idx])
+        return {
+            "img": img,
+            "label": label,
+            "idx": idx
+        }
+
+    def _load_img_label(self, path):
+        img = Image.open(os.path.join(self.root, "train2017" if self.train else "val2017", path))
+        label = Image.open(os.path.join(self.root,
+                                        "stuffthingmaps_trainval2017",
+                                        "train2017" if self.train else "val2017",
+                                        path.replace(".jpg", ".png")))
+        img = img.resize((self.resolution, self.resolution))
+        label = label.resize((self.resolution, self.resolution), resample=Image.NEAREST)
+        img = np.array(img).astype(np.float32) / 127.5 - 1
+        label = np.array(label).astype(np.uint8)
+        label = (label == self.category_id).astype(np.int64)
+
+        return img.transpose([2, 0, 1]), label
+
+    def support_set(self, selected_idx):
+        idx = set(range(self.__len__()))
+        idx.difference_update(set(selected_idx))
+        support_idx = random.choices(list(idx), k=self.num_support)
+        support_filenames = [self.filename_list[i] for i in support_idx]
+        support_img = []
+        support_label = []
+        for filename in support_filenames:
+            img, label = self._load_img_label(filename)
+            support_img.append(img)
+            support_label.append(label)
+        support_img = np.stack(support_img, axis=0)
+        support_label = np.stack(support_label, axis=0)
+        return support_img, support_label
+
+
+if __name__ == "__main__":
+    # a = [1, 4, 6, 7, 9]
+    # b = [1, 7]
+    # a = set(a)
+    # b = set(b)
+    # a.difference_update(b)
+    # print(list(a))
+
+    # coco = COCODataset(128, "../datasets/COCO", "person", 1, "../datasets/COCO/train_filenames.pkl")
+    # data = coco[0]
+    # print(1)
+    coco = COCOCategoryDataset(128, "../datasets/COCO", "../datasets/COCO/train_filenames.pkl",
+                               "../datasets/COCO/categories.pkl")
+    a = coco[0]
+    loader = DataLoader(a, batch_size=2, shuffle=True)
+    for b in loader:
+        s_img, s_label = a.support_set(b['idx'])
